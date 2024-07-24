f8f8f080f008d3c3c91ad6e3eca76dd371f49f14
unicorn <496300118@qq.com>
2024-07-24 11:43:57 +0800

    1.3 你好，三角形

--- No.1 Getting Started/1.3 Hello Triangle/src/main.cpp
+++ No.1 Getting Started/1.3 Hello Triangle/src/main.cpp
@@ -63,16 +63,16 @@ void PrepareShader() {
     glDeleteShader(fragmentShader);
 }
 
-unsigned int VAO;
-void PrepareData() {
+unsigned int VAO_Triangle;
+void PrepareTriangleData() {
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f,  0.5f, 0.0f
     };
     /* 1. 绑定顶点数组VAO */
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    glGenVertexArrays(1, &VAO_Triangle);
+    glBindVertexArray(VAO_Triangle);
     /* 2. 传输顶点数据VBO */
     unsigned int VBO;
     glGenBuffers(1, &VBO);
@@ -81,6 +81,46 @@ void PrepareData() {
     /* 3. 指定顶点属性位置 */
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    /* 4. 取消绑定 */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+unsigned int VAO_Square;
+void PrepareSquareData() {
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+    unsigned int indices[] = {
+        // 注意索引从0开始! 
+        // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+        // 这样可以由下标代表顶点组合成矩形
+
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    /* 1. 绑定顶点数组VAO */
+    glGenVertexArrays(1, &VAO_Square);
+    glBindVertexArray(VAO_Square);
+    /* 2. 传输顶点数据VBO */
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    /* 3. 传输索引数据EBO */
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    /* 4. 指定顶点属性位置 */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    /* 5. 取消绑定 */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
 void RenderLoop(GLFWwindow* window) {
@@ -89,8 +129,10 @@ void RenderLoop(GLFWwindow* window) {
     glClear(GL_COLOR_BUFFER_BIT);
     /* 绘制物体 */
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(VAO_Triangle);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO_Square);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 int main() {
@@ -121,7 +163,8 @@ int main() {
 
     /* 准备着色器&顶点数据 */
     PrepareShader();
-    PrepareData();
+    PrepareTriangleData();
+    PrepareSquareData();
 
     /* 渲染循环 */
     while (!glfwWindowShouldClose(window)) {
